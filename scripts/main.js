e3a375fdde8c53916e0f5057f057f5c764b7924a
VanilaGrey <VanilaGrey@users.noreply.github.com>
2023-05-08 18:50:23 +0000

    deploy: 80c8f686a02120912326be3df139a9d7bb9db185

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+!function(){"use strict";const t=document.querySelector(".promo__image-table"),e=document.querySelector(".promo__button--top"),o=document.querySelector(".promo__button--bottom");e.addEventListener("click",(()=>{t.classList.add("promo__lifted")})),o.addEventListener("click",(()=>{t.classList.remove("promo__lifted")}))}();
